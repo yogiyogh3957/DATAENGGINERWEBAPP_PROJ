07561468d56852a46a352c25feea4e0e7a78370c
yogianantaputra <yogi090891@gmail.com>
2021-11-14 12:22:58 +0700

    setup secret key and DB-1

--- fplapp/__init__.py
+++ fplapp/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 import os
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/fpldb"
-app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/fpldb"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "postgresql://postgres:password@localhost:5432/fpldb")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 db = SQLAlchemy(app)
 from fplapp import routes
